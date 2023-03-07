65ea9c38d98881b0cd4195f48a67f60addaf35ac
8 <f102m.8@gmail.com>
2023-03-08 01:21:46 +0330

    add files, add includefiles

---
+++ CA01-SoketProgramming/src/server.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <signal.h>
+
+#define STDIN 0
+#define STDOUT 1
+#define STDERR 2
+#define BUFFER_SIZE 1024
+
+
+
+
+int main(int argc, char const *argv[]) {
+    int server_fd;
+    int server_port;
+    
+}
