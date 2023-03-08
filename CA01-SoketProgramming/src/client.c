278abefa8c2c72cbb68972ce85ea5e4c20c1ef73
8 <f102m.8@gmail.com>
2023-03-08 12:49:13 +0330

    some initial changes

--- CA01-SoketProgramming/src/client.c
+++ CA01-SoketProgramming/src/client.c
@@ -1,1 +1,80 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+#include <signal.h>
+
+#define STDIN 0
+#define STDOUT 1
+#define STDERR 2
+
+#define BUFFER_SIZE 1024
+#define MAX_CONNECTION_TO_THE_SERVER 50
+
+#define TRUE  1
+#define FALSE 0
+
+#define NET_ADDRESS "127.0.0.1"
+
+#define STUDENT 1
+#define TA 2
+
+int role;
+//-----------------------------------------------------------
+
+int connect_to_server(int port) {
+    struct sockaddr_in server_address;
+    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    server_address.sin_family = AF_INET; 
+    server_address.sin_port = htons(port); 
+    server_address.sin_addr.s_addr = inet_addr(NET_ADDRESS);
+    
+    const char error[] = "Error in connecting\n";
+    if (connect(client_fd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0){
+        write(STDERR, error, sizeof(error));
+    }
+    else  { 
+        const char message[] = "Welcome!\n";
+        write(STDOUT, message, sizeof(message));
+    }
+    return client_fd;
+}
+
+void alarm_handler(int signal) {
+    char message[BUFFER_SIZE] = "Too late\n";
+    write(STDOUT, message, sizeof(message));
+}
+
+void set_role() {
+    char message[] = "1. student\n 2.TA\n";
+    write(STDOUT, message, sizeof(message));
+    read(STDIN, role, 1);
+}
+
+void TA_handler() {
+
+}
+void student_handler() {
+
+}
+
+int main(int argc, char const *argv) {
+    signal(SIGALRM, alarm_handler);
+    siginterrupt(SIGALRM, 1);
+
+    char buffer[BUFFER_SIZE] = {0};
+
+    int server_fd;
+
+    
+    set_role();
+
+    while(TRUE) {
+
+    }
+}   
--- CA01-SoketProgramming/src/server.c
+++ CA01-SoketProgramming/src/server.c
@@ -1,20 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <sys/time.h>
 #include <signal.h>
 
 #define STDIN 0
 #define STDOUT 1
 #define STDERR 2
+
 #define BUFFER_SIZE 1024
+#define MAX_CONNECTION_TO_THE_SERVER 50
 
+#define TRUE  1
+#define FALSE 0
 
+#define DEFAULT_PORT 8081
 
+int setupServer(int port) {
+    struct sockaddr_in address;
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0); //TCP
+    
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    
+    listen(server_fd, MAX_CONNECTION_TO_THE_SERVER);
+
+    const char message[] ="Server is running.../nWaiting for clients...";
+    write(STDOUT, message, sizeof(message));
+
+    return server_fd;
+}
+
+int accept_client(int server_fd) {
+    struct sockaddr_in client_address;
+    int address_len = sizeof(client_address);
+    int client_fd = accept(server_fd, (struct sockaddr *) &client_address, (socklen_t*) &address_len);
+
+    const char message[] = "New client connected\n";
+    write(STDOUT, message, sizeof(message));
+
+    return client_fd;
+}
 
 int main(int argc, char const *argv[]) {
-    int server_fd;
-    int server_port;
+    char buffer[BUFFER_SIZE] = {0};
     
+    if (argc <= 1) {
+        const char message[] = "server on default port...!";
+        write(STDOUT, message, sizeof(message));
+    }
+    int server_port = argc > 1 ? atoi(argv[1]) : DEFAULT_PORT;
+    int server_fd = setupServer(server_port);
+
+    fd_set master_set, working_set;
+    FD_ZERO(&master_set);
+    int max_fd = server_fd;
+    FD_SET(server_fd, &master_set);   
+
+    while(TRUE) {
+        working_set = master_set;
+        select(max_fd + 1, &working_set, NULL, NULL, NULL);
+        
+        for(int i = 0; i < max_fd + 1; i++) {
+
+            
+        }
+    }
 }
+
+
